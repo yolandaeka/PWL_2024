c4aff5f7e14396a5bc2395317873222c51d52e77
Your Name <you@example.com>
2024-08-28 16:06:26 +0700

    JS1 - KTP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PWL 2024</title>
 </head>
-<body>
-    <h2>Pemrograman web lanjut 2024</h2>
-    <h3>Semangat belajar laravel 10</h3>
+<body style="display: flex;
+            justify-content: center;
+            align-items: center;" >
+
+<div class="ktp">
+    <h2 class="ktp-title">PROVINSI JAWA TIMUR</h2>
+    <h2 class="ktp-title">KABUPATEN MALANG</h2>
+    <div class="ktp-info">
+        <div class="ktp-info">
+            <div class="NIK">
+                <span class="ktp-info-label">NIK</span> : 35030843100300010
+            </div>
+            <div class="ktp-info-item">
+                <span class="ktp-info-label">Nama</span> : YOLANDA EKAPUTRI SETYAWAN
+            </div>
+            <div class="ktp-info-item">
+                <span class="ktp-info-label">Tempat / Tgl Lahir</span> : MALANG, 18-05-2004
+            </div>
+            <div class="ktp-info-item">
+                <span class="ktp-info-label">Jenis Kelamin</span> : PEREMPUAN
+                <span class="ktp-info-label" style="margin-left: 1.8rem;">Gol Darah : O
+            </div>
+            <div class="ktp-info-item">
+                <span class="ktp-info-label">Alamat</span> : SAPTORAYA BLOK AE-01
+            </div>
+            <div class="ktp-info-item">
+                <span class="ktp-info-label">RT / RW</span> : 002/015
+            </div>
+            <div class="ktp-info-item">
+                <span class="ktp-info-label">Kel/Desa</span> : SAPTORENGGO
+            </div>
+            <div class="ktp-info-item">
+                <span class="ktp-info-label">Kecamatan</span> : PAKIS
+            </div>
+            <div class="ktp-info-item">
+                <span class="ktp-info-label">Agama</span> : ISLAM
+            </div>
+            <div class="ktp-info-item">
+                <span class="ktp-info-label">Status Perkawinan</span> : BELUM KAWIN
+            </div>
+            <div class="ktp-info-item">
+                <span class="ktp-info-label">Pekerjaan</span> : PELAJAR/MAHASISWA
+            </div>
+            <div class="ktp-info-item">
+                <span class="ktp-info-label">Kewarganegaraan</span> : WNI
+            </div>
+            <div class="ktp-info-item">
+                <span class="ktp-info-label">Berlaku Hingga</span> : SEUMUR HIDUP
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
